06f95c9be5c9ef435cd22ed0220425c9be11ef32
student06 <jakub.gola2@teb.edu.pl>
2025-03-31 15:07:30 +0200

    31.03.2025

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-def Euklines(a, b):
-    while a != b:
-        if a < b:
-            b -= a
-        else:
-            a -= b
-    print(a)
+#def Euklines(a, b):
+#    while a != b:
+#        if a < b:
+#            b -= a
+#        else:
+#            a -= b
+#    print(a)
+from math import floor
 
-def NWD(a, b):
-    while b:
-        t = b
-        b = a % b
-        a = t
-    print(a)
+
+#def NWD(a, b):
+#    while b:
+#        t = b
+#        b = a % b
+#        a = t
+#    print(a)
 
 # NWW(10, 5) = 10
 #(10 * 5) / NWW(10, 5) = 50/5 = 10
 
+def numeroUno(n):
+    for i in range(2, int(n ** 0,5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+
 if __name__ == '__main__':
-    a = int(input("Wpisz a: "))
-    b = int(input("Wpisz b: "))
-    print(Euklines(a, b))
-    print(NWD(a, b))
+#    a = int(input("Wpisz a: "))
+#    b = int(input("Wpisz b: "))
+    n = int(input("Wpisz n: "))
+#    print(Euklines(a, b))
+#    print(NWD(a, b))
